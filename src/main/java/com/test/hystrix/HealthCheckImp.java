9a63171d8341c3cd76000c9edab05c989c538aee
sai Vahin thoodi <saivahinthoodi@sais-MacBook-Air.local>
2016-09-18 21:19:00 -0400

    Hystrix application for registering

---
+++ src/main/java/com/test/hystrix/HealthCheckImp.java
@@ -0,0 +1,14 @@
+package com.test.hystrix;
+
+import org.springframework.cloud.netflix.hystrix.HystrixHealthIndicator;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by saivahinthoodi on 9/17/16.
+ */
+@Component
+public class HealthCheckImp{
+
+
+
+}
---
+++ src/main/java/com/test/hystrix/HystrixDemoApplication.java
@@ -0,0 +1,39 @@
+package com.test.hystrix;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@Configuration
+@ComponentScan
+@RestController
+@EnableHystrix
+@EnableDiscoveryClient
+public class HystrixDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixDemoApplication.class, args);
+	}
+
+	@HystrixCommand
+	@RequestMapping(value = "/test")
+	public String testApp() {
+		return "Hello Application";
+	}
+
+	@HystrixCommand
+	@RequestMapping(value = "/abcd")
+	public String testAppAB() {
+		return "Hello Application";
+	}
+
+
+}
